5fea983f72ec2ba3ff92a6ee88b184d586ec102e
Sarthakgite06 <sarthakgite006@gmail.com>
2026-09-11 18:29:24 +0530

    Add CORS middleware

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from Routes.StudentRoutes import router as student_router
 
-app = FastAPI(title="Student Management API")
+app = FastAPI()
+
+# CORS Configuration
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(student_router)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Student Management API is running successfully!"
+    }
